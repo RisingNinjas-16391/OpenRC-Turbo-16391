9b32a2c354b7bc8236eaf5c2388a48ba5d71f11d
Shxl2 <pl238412@ahschool.com>
2022-06-03 15:27:20 -0400

    Added Swerve

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TurnModules.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.drive.hardware;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.util.Encoder;
+
+public class TurnModules {
+    private CRServo turnServo;
+    private Encoder turnEncoder;
+
+    public void init(HardwareMap hwMap, String turnName, String encoderName) {
+        turnServo = hwMap.get(CRServo.class, turnName);
+        turnEncoder = hwMap.get(Encoder.class, encoderName);
+    }
+
+    public void setModuleOrientation(double targetAngle) {
+        double currentAngle = getModuleOrientation();
+        double error = closestAngle(targetAngle, currentAngle);
+        if (error > 10) {
+            turnServo.setPower(1);
+        } else if (error < -10) {
+            turnServo.setPower(-1);
+        } else {
+            turnServo.setPower(0);
+        }
+
+    }
+
+
+    private double closestAngle(double target, double current) {
+        double error = Math.toDegrees(target)-Math.toDegrees(current);
+        if (Math.abs(error) > 180) {
+            error = 360 - error;
+        }
+        return error;
+    }
+
+    public double getModuleOrientation() {
+        return Math.toRadians(turnEncoder.getCurrentPosition()*0.087891);
+
+    }
+}
