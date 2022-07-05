0ac3a5905b66f7093ce89e0c31ddca3d70504d63
A <local@MacBookAir-M1-AHS790.local>
2022-07-05 12:01:15 -0400

    Fixes stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/LiftSubsystem.java
@@ -7,29 +7,16 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class LiftSubsystem {
     private DcMotor motor;
-    public static final int TOP_TICK = -3500;
-    public static final int BOTTOM_TICK = 0;
-    public static final int DEADZONE = 20;
 
     public LiftSubsystem(HardwareMap hwMap) {
         motor = hwMap.get(DcMotor.class, "lift");
         motor.setDirection(DcMotorSimple.Direction.FORWARD);
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor.setDirection(DcMotorSimple.Direction.FORWARD);
         motor.setMode(RunMode.RUN_USING_ENCODER);
         motor.setPower(0);
     }
     public void setPower(double power) {
-        if (getCurrentPosition() < TOP_TICK) {
-            motor.setPower(0.005 * (TOP_TICK + DEADZONE - getCurrentPosition()));
-        }
-        else if (getCurrentPosition() > BOTTOM_TICK) {
-            motor.setPower(0.01 * (BOTTOM_TICK - DEADZONE - getCurrentPosition()));
-        } else if ((power > 0 && motor.getCurrentPosition() > BOTTOM_TICK - DEADZONE) || (power < 0 && motor.getCurrentPosition() < TOP_TICK + DEADZONE)) {
-            motor.setPower(0);
-        } else {
-            motor.setPower(power);
-        }
+        motor.setPower(power * 0.7);
     }
     public void reset() {
         motor.setMode(RunMode.STOP_AND_RESET_ENCODER);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ResetPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/ResetPosition.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.drive.Hardware;
 
+@Disabled
 @TeleOp(name = "Reset Lift", group = "Teleop")
 public class ResetPosition extends LinearOpMode {
 
